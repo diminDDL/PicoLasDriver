5c9759120eb208dfa2e42871adc623e55426709a
diminDDL <dimin21590@gmail.com>
2023-02-17 23:08:43 +0100

    Added minimal EEPROM code

--- LaserControl/src/main.cpp
+++ LaserControl/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include "pwm_lib.h"
 #include "tc_lib.h"
-#include "Wire.h"
+#include <DueFlashStorage.h>
 
 using namespace arduino_due::pwm_lib;
 // create a wire object
+
+// TODO
+// DAC
+// local pulse count
+// EEPROM
 
 // configuration variables
 // set serial port to 115200 baud, data 8 bits, even parity, 1 stop bit
@@ -23,12 +28,32 @@ using namespace arduino_due::pwm_lib;
 #define E_STOP_LED A3                       // LED indicating that the e-stop is halting the operation of the board
 #define FAULT_LED A2                        // LED indicating an internal fault
 PULSE_PIN pulsePin;                         // pulse pin object
+DueFlashStorage EEPROM;            // EEPROM object
 
 // if estop is true the driver will stop sending pulses to the laser
+
+// the settings we want to store in the EEPROM:
+// globalPulseCount
+// setCurrent
+// maxCurrent
+// setPulseDuration
+// setPulseFrequency
+// analogMode
+// the rest are reset on power cycle
+struct __attribute__((__packed__)) Configuration {
+    uint64_t globalpulse;
+    float current;
+    float maxcurr;
+    uint32_t pulsedur;
+    uint32_t pulsefreq;
+    bool analog;
+};
 
 // variables that need to be remembered between power cycles
 uint64_t globalPulseCount = 0;                          // counts the total number of pulses sent do the driver
 const String getGlobalPulseCountCommand = "gpsc";       // string representation of the pulse count command
+uint64_t localPulseCount = 0;                           // counts the number of pulses sent to the driver since it was turned on
+const String getLocalPulseCountCommand = "gcpc";        // string representation of the local pulse count command
 float setCurrent = 0.0;                                 // current set by the user in Amperes
 const String setCurrentCommand = "scur";                // string representation of the set current command
 const String getCurrentCommand = "gcur";                // string representation of the get current command
@@ -99,7 +124,62 @@ void trg(){
 }
 
 void pulse(){
-    globalPulseCount++;
+    localPulseCount++;
+}
+
+void print_big_int(uint64_t value)
+{
+    if ( value >= 10 )
+    {
+        print_big_int(value / 10);
+    }
+    
+    Serial.print((uint32_t)(value % 10));
+}
+
+#define CRC16 0x8005
+
+uint16_t gen_crc16(const uint8_t *data, uint16_t size)
+{
+    uint16_t out = 0;
+    int bits_read = 0, bit_flag;
+    /* Sanity check: */
+    if(data == NULL)
+        return 0;
+    while(size > 0)
+    {
+        bit_flag = out >> 15;
+        /* Get next bit: */
+        out <<= 1;
+        out |= (*data >> bits_read) & 1; // item a) work from the least significant bits
+        /* Increment bit counter: */
+        bits_read++;
+        if(bits_read > 7)
+        {
+            bits_read = 0;
+            data++;
+            size--;
+        }
+        /* Cycle check: */
+        if(bit_flag)
+            out ^= CRC16;
+    }
+    // item b) "push out" the last 16 bits
+    int i;
+    for (i = 0; i < 16; ++i) {
+        bit_flag = out >> 15;
+        out <<= 1;
+        if(bit_flag)
+            out ^= CRC16;
+    }
+    // item c) reverse the bits
+    uint16_t crc = 0;
+    i = 0x8000;
+    int j = 0x0001;
+    for (; i != 0; i >>=1, j <<= 1) {
+        if (i & out) crc |= j;
+    }
+    return crc;
 }
 
 void setup() {
@@ -123,8 +203,42 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), trg, RISING);
     pinMode(6, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(6), pulse, RISING);
-    Wire1.begin();
-    Wire1.setClock(400000);
+    Serial.println("Reading EEPROM");
+    byte* b = EEPROM.readAddress(4);
+    Configuration c;
+    memcpy(&c, b, sizeof(Configuration));
+    // print the values
+    Serial.print("globalpulse: ");
+    print_big_int(c.globalpulse);
+    Serial.println();
+    Serial.print("current: ");
+    Serial.println(c.current);
+    Serial.print("maxcurrent: ");
+    Serial.println(c.maxcurr);
+    Serial.print("pulseduration: ");
+    Serial.println(c.pulsedur);
+    Serial.print("pulsefrequency: ");
+    Serial.println(c.pulsefreq);
+    // read the CRC
+    Serial.print("CRC: ");
+    uint16_t crcRead = (EEPROM.read(1) << 8) | EEPROM.read(0);
+    Serial.println(crcRead, HEX);
+    // calculate the CRC
+    uint16_t crcCalc = gen_crc16(b, sizeof(Configuration));
+    Serial.print("CRC calc: ");
+    Serial.println(crcCalc, HEX);
+    // check if the CRC is correct
+    if (crcRead == crcCalc){
+        Serial.println("CRC correct");
+        // set the values
+        globalPulseCount = c.globalpulse;
+        setCurrent = c.current;
+        maxCurrent = c.maxcurr;
+        setPulseDuration = c.pulsedur;
+        setPulseFrequency = c.pulsefreq;
+    } else {
+        Serial.println("CRC incorrect");
+    }
 }
 
 void printErrorStr(bool commandType = false){
@@ -147,16 +261,6 @@ void printErrorStr(bool commandType = false){
     if (commandType){
         valuesChanged = true;
     }
-}
-
-void print_big_int(uint64_t value)
-{
-    if ( value >= 10 )
-    {
-        print_big_int(value / 10);
-    }
-    
-    Serial.print((uint32_t)(value % 10));
 }
 
 // Reads the Serial port and stores it into dest.
@@ -293,6 +397,11 @@ void parser(char str[]){
             print_big_int(globalPulseCount);
             Serial.print(EOL);
             printErrorStr();
+        } else if (strcmp(str, getLocalPulseCountCommand.c_str()) == 0){
+            // get the local pulse count
+            print_big_int(localPulseCount);
+            Serial.print(EOL);
+            printErrorStr();
         } else if (strcmp(str, getCurrentCommand.c_str()) == 0){
             // get the current
             Serial.print(setCurrent);
@@ -383,6 +492,66 @@ void set_values(){
     }
 }
 
+void EEPROM_service(){
+    static uint32_t lastTime = 0;
+    uint16_t crc;
+    Configuration conf;
+    // if 
+    if (millis() - lastTime > 1000 || valuesChanged){
+        lastTime = millis();
+        Serial.println("Checking if needs to be saved to EEPROM");
+
+        // read CRC from EEPROM
+        Serial.print("CRC EEPROM: ");
+        uint16_t crcRead = (EEPROM.read(1) << 8) | EEPROM.read(0);
+        Serial.println(crcRead, HEX);
+        // set the values to the struct
+        conf.globalpulse = globalPulseCount;
+        conf.current = setCurrent;
+        conf.maxcurr = maxCurrent;
+        conf.pulsedur = setPulseDuration;
+        conf.pulsefreq = setPulseFrequency;
+        conf.analog = analogMode;
+        // calculate the CRC
+        byte b2[sizeof(Configuration)]; // create byte array to store the struct
+        memcpy(b2, &conf, sizeof(Configuration)); // copy the struct to the byte array
+        // calculate the crc
+        crc = gen_crc16(b2, sizeof(Configuration)); 
+        // print the crc
+        Serial.print("CRC: ");
+        Serial.println(crc, HEX);
+        if(crcRead != crc){
+            // print the values
+            Serial.print("globalpulse: ");
+            print_big_int(conf.globalpulse);
+            Serial.println();
+            Serial.print("current: ");
+            Serial.println(conf.current);
+            Serial.print("maxcurrent: ");
+            Serial.println(conf.maxcurr);
+            Serial.print("pulseduration: ");
+            Serial.println(conf.pulsedur);
+            Serial.print("pulsefrequency: ");
+            Serial.println(conf.pulsefreq);
+            Serial.print("analog: ");
+            Serial.println(conf.analog);
+
+            // write byte array to flash
+            EEPROM.write(4, b2, sizeof(Configuration));
+            Serial.println("Writing to EEPROM");
+            // print out each byte
+            for (int i = 0; i < sizeof(Configuration); i++){
+                Serial.print(b2[i], HEX);
+                Serial.print(" ");
+            }
+            Serial.println();
+            // write the crc to byte 0 and 1
+            EEPROM.write(0, crc & 0xFF);
+            EEPROM.write(1, (crc >> 8) & 0xFF);
+        }
+    }
+}
+
 void loop() {
     read_host_data(hostBuffer, sizeof(hostBuffer));
     if(newData){
@@ -393,9 +562,11 @@ void loop() {
         parser(hostBuffer);
     }
     delay(100);
+    EEPROM_service();
     if(!estop){
         if(valuesChanged){
             set_values();
+            EEPROM_service();
             valuesChanged = false;
         }
     }else{
@@ -412,52 +583,6 @@ void loop() {
     //     Serial.println();
     // }
     digitalWrite(OE_LED, outputEnabled);
-
-
-    // Wire1.beginTransmission(0x60);
-    // Wire1.write(0x40);
-    // Wire1.write(0x55);
-    // Wire1.write(0x00);
-    // Wire1.endTransmission(true);
-
-      byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire1.beginTransmission(address);
-    error = Wire1.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(100);           // wait 5 seconds for next scan
 }
 
 
